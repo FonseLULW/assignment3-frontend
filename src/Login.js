f861735ba7e57dca4395d053fec67eeb7b5807d4
FonseLULW <jalfonsclarito@gmail.com>
2023-04-08 12:55:59 -0700

    Created login UI

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Text } from '@chakra-ui/react'
+import { Box, Text, Center, Square } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input
+  } from '@chakra-ui/react'
 
 function Login() {
     return (
-        <>
-            <Text>LOGIN</Text>
-        </>
+        <Center h='100%'>
+            <Box bg='#FFFFFF' w='20%' p={4} borderColor='gray.200' border='1px'>
+                <Text fontSize='3xl' mb={10}>Login</Text>
+                <FormControl isRequired>
+                    <FormLabel>Username</FormLabel>
+                    <Input placeholder='Username' />
+
+                    <FormLabel mt={2}>Password</FormLabel>
+                    <Input type="password"/>
+                </FormControl>
+            </Box>
+        </Center>
     )
 }
 
